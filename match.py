26bd8a8aa0df85e487c4a5ac7fc698940c30765e
Eason <47597085+Lalalaashen@users.noreply.github.com>
2020-02-18 19:39:22 +0800

    add

--- match.py
+++ match.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-
 import pandas as pd
+from tqdm import trange
 
 
 def read_and_chop(file_name, sheet_name=0) -> pd.DataFrame:
@@ -11,34 +11,68 @@ def read_and_chop(file_name, sheet_name=0) -> pd.DataFrame:
     return df
 
 
-# 去除最后两行
-top_ten_df = read_and_chop('股东情况.xlsx', '前十大股东')
-detail_df = read_and_chop('股东情况.xlsx', '股东')
+def match(top_ten_df, detail_df):
+    pe_ratio_ls = []
+    type_ls = []
 
+    for i in range(1, 11):
+        col = f'第{i}大股东'
+        ratio_col = f'第{i}大股东持股比例'
+        temp = top_ten_df[[col, ratio_col]].reset_index().merge(
+            detail_df[['股东', '性质']],
+            left_on=col,
+            right_on='股东',
+        ).set_index('index')
+        pe_ratio_ls.append(temp[ratio_col])
+        type_ls.append(temp['性质'])
+
+    top_ten_ratio = top_ten_df[[f'第{i}大股东持股比例' for i in range(1, 11)]].sum(axis=1)
+
+    pe_ratio_df = pd.concat(pe_ratio_ls, axis=1)
+    type_df = pd.concat(type_ls, axis=1)
+    pe_num = pd.notna(pe_ratio_df).sum(axis=1).rename('PE_NUM')
+    pe_ratio = pe_ratio_df.sum(axis=1).rename('PE_RATIO')
+    state_num = (type_df == '国有').sum(axis=1).rename('STATE_NUM')
+    state_ratio = ((type_df == '国有').values * pe_ratio_df).sum(axis=1).rename('STATE_RATIO')
+    state_over_pe_num = (state_num / pe_num).rename('STATE_OVER_PE_NUM')
+    state_over_pe_ratio = (state_ratio / pe_ratio).rename('STATE_OVER_PE_RATIO')
+
+    result = pd.concat(
+        objs=[
+            pe_num,
+            pe_ratio,
+            state_num,
+            state_ratio,
+            state_over_pe_num,
+            state_over_pe_ratio,
+        ],
+        axis=1,
+    ).fillna(0)  # type: pd.DataFrame
+    result['PE_OVER_TEN_RATIO'] = result['PE_RATIO'] / top_ten_ratio
+    return result
+
+
+# 去除最后两行
+detail_df = read_and_chop('股东情况.xlsx', '股东')
 detail_df = detail_df[
     (detail_df['类型'] == 'PE')
     | (detail_df['类型'] == 'VC')
     | (detail_df['类型'] == '战略投资者')
     ]
 
-merged_df_ls = []
-for i in range(1, 11):
-    col = f'第{i}大股东'
-    ratio_col = f'第{i}大股东持股比例'
-    merged_df = top_ten_df[[col, ratio_col]].reset_index().merge(
-        detail_df['股东'],
-        left_on=col,
-        right_on='股东',
-    ).set_index('index')
-    merged_df_ls.append(merged_df[ratio_col])
+res_ls = []
+input_io = pd.ExcelFile('股东情况.xlsx')
+for i in trange(4):
+    top_ten_df = read_and_chop(input_io, f't{i}前十大股东')
+    res = match(top_ten_df, detail_df)[[
+        'PE_NUM', 'PE_RATIO'
+    ]]
+    res = res.rename(columns={
+        'PE_NUM': f'PE_NUM_{i}',
+        'PE_RATIO': f'PE_RATIO_{i}',
+    })
+    res_ls.append(res)
 
-total_merged_df = pd.concat(merged_df_ls, axis=1)
-result = pd.concat(
-    objs=[
-        top_ten_df.iloc[:, :5],
-        pd.notna(total_merged_df).sum(axis=1).rename('数量'),
-        total_merged_df.sum(axis=1).rename('总占比'),
-    ],
-    axis=1,
-)  # type: pd.DataFrame
+result = pd.concat([top_ten_df.iloc[:, :5]] + res_ls, axis=1).fillna(0)
 result.to_excel('股东情况匹配.xlsx')
+# input_io.close()
---
+++ regression.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+# %% Init
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+from sklearn.preprocessing import OneHotEncoder
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+
+
+def read_and_chop(file_name, sheet_name=0) -> pd.DataFrame:
+    """读取并截取有效数据"""
+    df = pd.read_excel(file_name, sheet_name=sheet_name)
+    if df.iloc[-1, 0] == '数据来源：Wind':
+        df = df.iloc[:-2]
+    return df
+
+
+def get_dummy(ind):
+    """生成哑变量"""
+    ohe = OneHotEncoder()
+    ohe.fit(ind)
+    return ohe.transform(ind).toarray()
+
+
+# %% 读取
+Xs = read_and_chop('重大重组事件V3_无st.xlsx', '长期绩效变量').fillna(0)
+# Xs = read_and_chop('重大重组事件V4_无st.xlsx', '长期绩效变量').fillna(0)
+
+# # %% 数据clip
+controlled = ['RELATIVE SIZE', 'LN_ASSET', 'LEV', 'BROE', 'CASHPAY', 'OCF']
+# clip
+for c in controlled:
+    Xs[c] = Xs[c].clip(*np.percentile(Xs[c], [1, 99]))
+
+# %% 数据处理
+# 去除行业数量太小的
+ind_clip_num = 1
+ind_value_counts = Xs['INDU'].value_counts()
+others = ind_value_counts[ind_value_counts <= ind_clip_num].index
+for other in others:
+    Xs.loc[Xs['INDU'] == other, 'INDU'] = '其他'
+# print(Xs['INDU'].value_counts())
+
+# %% 生成哑变量
+# 行业哑变量
+ind_ohe_trans = get_dummy(Xs['INDU'].values.reshape((-1, 1)))[:, :-1]  # 以最后一个为基准
+for i in range(ind_ohe_trans.shape[1]):
+    Xs['IND_{}'.format(i)] = ind_ohe_trans[:, i]
+
+# %% 回归并打印结果
+while True:
+    # %% 读取X
+    # to_add = [
+    #     'PE_1',
+    #     'PE_UNITED',
+    #     # 'PE_RATIO',
+    #     # 'STATE_NUM',
+    #     # 'STATE_RATIO',
+    #     # 'STATE_OVER_PE_NUM',
+    #     'STATE_OVER_PE_RATIO',
+    #     # 'PE_OVER_TEN_RATIO',
+    # ]
+    to_add = input('输入变量名，空格分开').split(' ')
+    ind_dummy = ['IND_{}'.format(i) for i in range(ind_ohe_trans.shape[1])]
+    xs_cols = controlled + to_add + ind_dummy
+    xs = Xs[xs_cols]
+    x = sm.add_constant(xs)  # 若模型中有截距
+    cared = to_add
+    cared_index = [xs_cols.index(x) + 1 for x in cared]  # 带常量的index
+
+    y_cols = ['t0-t-1', 't1-t-1', 't2-t-1', 't3-t-1']
+    df = pd.DataFrame(
+        index=cared,
+        columns=pd.MultiIndex.from_product([y_cols, ['pvalue', 'beta']])
+    )
+    print(cared)
+    for col in y_cols:
+        y = Xs[col]
+        model = sm.OLS(y, x).fit()  # 构建最小二乘模型并拟合
+        # print(model.summary())  # 输出所有回归结果
+        df[(col, 'pvalue')] = model.pvalues[cared_index].values
+        df[(col, 'beta')] = model.params[cared_index].values
+    print(df)
